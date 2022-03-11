11c4cd9ae44d40ba61eff73fa0508c81d2c7d3fc
Mayan Rothstein <mayan.rothstein@umusic.com>
2022-03-10 21:02:12 -0800

    render list of search results

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
+const VideoList = ({ videos }) => {
     // props.videos is the array that comes through
-    return <div>{props.videos.length}</div>;
+    const renderedList = videos.map((video) => {
+        // pass each video as a prop to the VideoItem component
+        return <VideoItem video={video}/>;
+    });
+    return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
